cc3a439d1cf22dd3974f9dd7dfd86f692d9ab5a1
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-07-30 19:13:26 +0530

    Delete run_project.sh

--- run_project.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-REPO_SSH="git@github.com:your-username/your-repo.git"
-git clone "$REPO_SSH" myapp
-cd myapp
-nohup java -jar build/libs/project.jar > logs.txt 2>&1 &
-echo "App running on http://localhost:9000"
